4e086fe98bb89ad555a37e5ae882fd56c5e31981
lha288733-bit <lha288733@gmail.com>
2026-05-05 08:36:40 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f"Đã đăng nhập: {bot.user}")
   
-    channel_id = 1490092888539926711  
+    channel_id = 1496702028750131363  
 
     channel = bot.get_channel(channel_id)
 
